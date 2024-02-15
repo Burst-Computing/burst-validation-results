0ee208ce3a704989f6494577ebca15c74543fc4b
Aitor Arjona <aitor.a98@gmail.com>
2024-02-15 12:03:06 +0100

    Fix plots

--- pairs2/pairs2.py
+++ pairs2/pairs2.py
@@ -32,31 +32,46 @@ FILES = [
     "pairs2/message-relay.csv",
 ]
 
+PAYLOAD_SIZE = (1073741824 / 1024) / 1024  # 1GB in MB
+
 
 if __name__ == "__main__":
     dfs = []
-    for file, (chunk_size, chunk_size_str) in zip(FILES, CHUNK_SIZES):
+    for file in FILES:
         tmp_df = pd.read_csv(file)
+        print(file)
         dfs.append(tmp_df)
 
     df = pd.concat(dfs, ignore_index=True, axis=0)
 
-    data_latency = defaultdict(list)
-    data_latency_std_dev = defaultdict(list)
+    Y_lat = defaultdict(list)
+    Y_lat_stddev = defaultdict(list)
     data_throughput = defaultdict(list)
     data_throughput_std_dev = defaultdict(list)
     for backend in BACKENDS.keys():
         for chunk_size, chunk_size_label in CHUNK_SIZES:
             tmp_df = df[(df["chunk_size"] == chunk_size) & (df["backend"] == backend)]
-            latencies = tmp_df["end"] - tmp_df["start"]
+            if tmp_df.empty:
+                print(f"Backend: {backend}, Chunk Size: {chunk_size} is empty. Skipping...")
+                Y_lat[backend].append(0.0)
+                Y_lat_stddev[backend].append(0.0)
+                data_throughput[backend].append(0.0)
+                data_throughput_std_dev[backend].append(0.0)
+                continue
+
+            t_start = tmp_df[tmp_df["group_id"] == 0][["burst_id", "start"]]
+            t_end = tmp_df[tmp_df["group_id"] == 1][["burst_id", "end"]]
+
+            df_times = pd.merge(t_start, t_end, on="burst_id")
+            latencies = df_times["end"] - df_times["start"]
             latency_avg = np.median(latencies)
             latency_std_dev = np.std(latencies)
 
             print(f"{backend} Latency ({chunk_size_label}): {latency_avg:.2f} s ± {latency_std_dev:.2f}")
-            data_latency[backend].append(latency_avg)
-            data_latency_std_dev[backend].append(latency_std_dev)
+            Y_lat[backend].append(latency_avg)
+            Y_lat_stddev[backend].append(latency_std_dev)
 
-            throughputs = tmp_df["throughput"]
+            throughputs = PAYLOAD_SIZE / latencies
             throughput_avg = np.median(throughputs)
             throughput_std_dev = np.std(throughputs)
 
@@ -70,7 +85,7 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
     ax.grid(axis="y", linestyle="--", alpha=0.7, zorder=0)
     kwargs = {
-        "width": 0.15,
+        "width": 0.1,
         "linewidth": 1,
         "edgecolor": "black",
         "align": "center",
@@ -79,16 +94,29 @@ if __name__ == "__main__":
         "capsize": 3,
         "zorder": 3,
     }
-    ax.bar(X - 0.45, data_latency["RabbitMQ"], yerr=data_latency_std_dev["RabbitMQ"], label="RabbitMQ", **kwargs)
-    ax.bar(X - 0.3, data_latency["RedisList"], yerr=data_latency_std_dev["RedisList"], label="RedisList", **kwargs)
-    ax.bar(X - 0.15, data_latency["DragonflyList"], yerr=data_latency_std_dev["DragonflyList"], label="DragonflyList", **kwargs)
-    ax.bar(X, data_latency["RedisStream"], yerr=data_latency_std_dev["RedisStream"], label="RedisStream", **kwargs)
-    ax.bar(X + 0.15, data_latency["DragonflyStream"], yerr=data_latency_std_dev["DragonflyStream"], label="DragonflyStream", **kwargs)
-    ax.bar(X + 0.3, data_latency["S3"], yerr=data_latency_std_dev["S3"], label="S3", **kwargs)
+
+    ax.bar(X - 0.3, Y_lat["RabbitMQ"], yerr=Y_lat_stddev["RabbitMQ"], label="RabbitMQ", **kwargs)
+    ax.bar(X - 0.2, Y_lat["RedisList"], yerr=Y_lat_stddev["RedisList"], label="RedisList", **kwargs)
     ax.bar(
-        X + 0.45,
-        data_latency["BurstMessageRelay"],
-        yerr=data_latency_std_dev["BurstMessageRelay"],
+        X - 0.1,
+        Y_lat["DragonflyList"],
+        yerr=Y_lat_stddev["DragonflyList"],
+        label="DragonflyList",
+        **kwargs,
+    )
+    ax.bar(X, Y_lat["RedisStream"], yerr=Y_lat_stddev["RedisStream"], label="RedisStream", **kwargs)
+    ax.bar(
+        X + 0.1,
+        Y_lat["DragonflyStream"],
+        yerr=Y_lat_stddev["DragonflyStream"],
+        label="DragonflyStream",
+        **kwargs,
+    )
+    ax.bar(X + 0.2, Y_lat["S3"], yerr=Y_lat_stddev["S3"], label="S3", **kwargs)
+    ax.bar(
+        X + 0.3,
+        Y_lat["BurstMessageRelay"],
+        yerr=Y_lat_stddev["BurstMessageRelay"],
         label="MessageRelay",
         **kwargs,
     )
@@ -105,7 +133,7 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
     ax.grid(axis="y", linestyle="--", alpha=0.7, zorder=0)
     kwargs = {
-        "width": 0.15,
+        "width": 0.1,
         "linewidth": 1,
         "edgecolor": "black",
         "align": "center",
@@ -114,31 +142,35 @@ if __name__ == "__main__":
         "capsize": 3,
         "zorder": 3,
     }
-    ax.bar(X - 0.45, data_throughput["RabbitMQ"], yerr=data_throughput_std_dev["RabbitMQ"], label="RabbitMQ", **kwargs)
+    ax.bar(X - 0.3, data_throughput["RabbitMQ"], yerr=data_throughput_std_dev["RabbitMQ"], label="RabbitMQ", **kwargs)
     ax.bar(
-        X - 0.3, data_throughput["RedisList"], yerr=data_throughput_std_dev["RedisList"], label="RedisList", **kwargs
+        X - 0.2, data_throughput["RedisList"], yerr=data_throughput_std_dev["RedisList"], label="RedisList", **kwargs
     )
     ax.bar(
-        X - 0.15, data_throughput["DragonflyList"], yerr=data_throughput_std_dev["DragonflyList"], label="DragonflyList", **kwargs
+        X - 0.1,
+        data_throughput["DragonflyList"],
+        yerr=data_throughput_std_dev["DragonflyList"],
+        label="DragonflyList",
+        **kwargs,
     )
     ax.bar(
         X, data_throughput["RedisStream"], yerr=data_throughput_std_dev["RedisStream"], label="RedisStream", **kwargs
     )
     ax.bar(
-        X + 0.15, data_throughput["DragonflyStream"], yerr=data_throughput_std_dev["DragonflyStream"], label="DragonflyStream", **kwargs
+        X + 0.1,
+        data_throughput["DragonflyStream"],
+        yerr=data_throughput_std_dev["DragonflyStream"],
+        label="DragonflyStream",
+        **kwargs,
     )
-    ax.bar(X + 0.3, data_throughput["S3"], yerr=data_throughput_std_dev["S3"], label="S3", **kwargs)
+    ax.bar(X + 0.2, data_throughput["S3"], yerr=data_throughput_std_dev["S3"], label="S3", **kwargs)
     ax.bar(
-        X + 0.45,
+        X + 0.3,
         data_throughput["BurstMessageRelay"],
         yerr=data_throughput_std_dev["BurstMessageRelay"],
         label="MessageRelay",
         **kwargs,
     )
-    # ax.bar(
-    #     X + 0.15, data_latency["RedisStream"], yerr=data_latency_std_dev["RedisStream"], label="RedisList", **kwargs
-    # )
-    # ax.bar(X + 0.3, data_latency["RedisStream"], yerr=data_latency_std_dev["RedisStream"], label="RedisList", **kwargs)
 
     ax.set_xticks(X)
     ax.set_xticklabels(X_labels)
